e58060642d25c7af7c0367c6b76627097f79ca9e
PunditG <pundit.bandit@gmail.com>
2014-12-29 01:00:43 -0700

    adding summary functionality

---
+++ vShare/Summary.swift
@@ -0,0 +1,13 @@
+//
+//  Summary.swift
+//  vShare
+//
+//  Created by Sulabh Shukla on 12/28/14.
+//  Copyright (c) 2014 eva. All rights reserved.
+//
+
+import UIKit
+
+class Summary: NSObject {
+   
+}
